cfe6e2dc0bfc593589700f3b13dd75f4c20a857c
guilherme3110 <guilherme311094@hotmail.com>
2023-02-17 15:15:00 -0300

    mexendo da requests da api

---
+++ src/scripts/search.js
@@ -0,0 +1,11 @@
+import { getCharacterByName } from "./requests.js"
+
+
+export function searchUser(){
+    const search = document.querySelector('.')
+    const btn = document.querySelector('.')
+
+    btn.addEventListener('click', (e) => {
+        
+    })
+}
